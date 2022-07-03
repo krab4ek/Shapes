557d7b7365af9df63073068a4ca09ff31a4b1757
Covali Andrei <krab4ek@gmail.com>
2022-07-03 19:33:15 +0300

    final

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -8,11 +8,19 @@ hex.Draw();
 Circle circle = new Circle("Cindy");
 circle.Draw();
 
+Console.WriteLine();
+
 Shape[] myShapes = { new Hexagon(), new Circle(),
     new Hexagon("Lily"),new Circle("Alla"), new Hexagon("Tratra") };
 foreach (Shape myShape in myShapes)
 {
     myShape.Draw();
 }
+
+Console.WriteLine();
+
+ThreeDCircle trid = new ThreeDCircle();
+trid.Draw();
+((Circle)trid).Draw();
 
 Console.ReadLine();
---
+++ Shapes/ThreeDCircle.cs
@@ -0,0 +1,12 @@
+﻿namespace Shapes
+{
+    internal class ThreeDCircle : Circle
+    {
+        public new string? PetName { get; set; }
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing a 3D Circle");
+        }
+
+    }
+}
